f88ca88cf7affecd7bdc116adcd3ff532162af50
unknown <uraz@sistema.at>
2024-10-15 14:47:42 +0200

    Added a legend for more information.

--- storm_surges/frontend/src/app/components/Map.tsx
+++ storm_surges/frontend/src/app/components/Map.tsx
@@ -30,6 +30,7 @@ const Map = () => {
   const [selectedYear, setSelectedYear] = useState("2150");
   const [stormSurge, setStormSurge] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
+  const [showLegend, setShowLegend] = useState(false);
 
   // Function to cache and retrieve tile URLs
   const getCachedTileUrl = (
@@ -128,6 +129,10 @@ const Map = () => {
     }
   }, [showOverlayLayer]);
 
+  const toggleLegend = () => {
+    setShowLegend((prev) => !prev); // Toggle the visibility of the legend
+  };
+
   const basePath = process.env.BASEPATH || "";
 
   return (
@@ -195,29 +200,30 @@ const Map = () => {
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         )}
         {showOverlayLayer && (
-         <WMSTileLayer
-         url="https://hulk.adamplatform.eu/wmts?"
-         layers="InundationMap"
-         styles="InundationMap;colorrange=(0,2);nodata=2"
-         format="image/png"
-         transparent={false}
-         version="1.3.0"
-         crs={L.CRS.EPSG4326}
-         opacity={0.5}
-         params={{
-           time: `${selectedYear}-12-31T00:00:00Z,${selectedYear}-12-31T23:59:59Z`,
-           bbox: "44.20,11.73,45.94,14.18",
-           token: "bf12d6193efa667283ee9643951acfaa",
-           ssp: selectedSSP,
-           confidence: confidenceLevel.toLowerCase(),
-           stormSurge: formatStormSurge(stormSurge),
-         }as any}
-         eventHandlers={{
-           loading: () => setIsLoading(true),
-           load: () => setIsLoading(false),
-         }}
-       />
-       
+          <WMSTileLayer
+            url="https://hulk.adamplatform.eu/wmts?"
+            layers="InundationMap"
+            styles="InundationMap;colorrange=(0,2);nodata=2"
+            format="image/png"
+            transparent={false}
+            version="1.3.0"
+            crs={L.CRS.EPSG4326}
+            opacity={0.5}
+            params={
+              {
+                time: `${selectedYear}-12-31T00:00:00Z,${selectedYear}-12-31T23:59:59Z`,
+                bbox: "44.20,11.73,45.94,14.18",
+                token: "bf12d6193efa667283ee9643951acfaa",
+                ssp: selectedSSP,
+                confidence: confidenceLevel.toLowerCase(),
+                stormSurge: formatStormSurge(stormSurge),
+              } as any
+            }
+            eventHandlers={{
+              loading: () => setIsLoading(true),
+              load: () => setIsLoading(false),
+            }}
+          />
         )}
       </MapContainer>
       <button
@@ -232,6 +238,9 @@ const Map = () => {
           textAlign: "center",
           cursor: "pointer",
           marginTop: "5px",
+          right: "10px",
+          position: "absolute",
+          top: "80px", // Position this button 10px from the top
         }}
       >
         <Image
@@ -241,7 +250,80 @@ const Map = () => {
           height="21"
         />
       </button>
-      {isCursorOnMap && (
+
+      {/* Additional Button Below */}
+      <button
+        className="leaflet-control-custom"
+        onClick={toggleLegend} // Change the handler if needed
+        style={{
+          background: "white",
+          color: "black",
+          border: "1px solid black",
+          borderRadius: "5px",
+          padding: "5px",
+          textAlign: "center",
+          cursor: "pointer",
+          marginTop: "5px",
+          right: "10px",
+          position: "absolute",
+          top: "120px",
+        }}
+      >
+        <Image src={basePath + "/info.svg"} alt="Info" width="21" height="21" />
+      </button>
+      {/* Legend window */}
+      {showLegend && (
+        <div
+          style={{
+            position: "absolute",
+            top: "125px", // Same top as the button
+            right: "45px", // Positioned to the left of the button
+            background: "white",
+            border: "1px solid black",
+            borderRadius: "5px",
+            padding: "10px",
+            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+            zIndex: 9999, // Ensure it appears above other elements
+            width: "200px", // Adjust size as needed
+            fontSize: "14px",
+            color: "black",
+            opacity: '70%',
+          }}
+        >
+          {/* Legend content */}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "5px",
+            }}
+          >
+            <div
+              style={{
+                width: "20px",
+                height: "20px",
+                backgroundColor: "#78F773", // Green square
+                marginRight: "10px",
+              }}
+            ></div>
+            <span>Protected areas <br></br>(2020 baseline)</span>
+          </div>
+
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <div
+              style={{
+                width: "20px",
+                height: "20px",
+                backgroundColor: "#F67769", // Red square
+                marginRight: "10px",
+              }}
+            ></div>
+            <span>Areas at risk of flood</span>
+          </div>
+        </div>
+      )}
+
+      {/* {isCursorOnMap && (
         <div
           style={{
             position: "absolute",
@@ -256,7 +338,7 @@ const Map = () => {
             fontSize: "12px",
           }}
         ></div>
-      )}
+      )} */}
     </div>
   );
 };
